ddb0cf54978c632b06f483706df50a427327090e
Artur Agh <aghartur@gmail.com>
2025-05-06 10:15:44 +0200

    login page styled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,13 @@
-import {Component, inject} from '@angular/core';
-import {ProfileCardComponent} from './common-ui/profile-card/profile-card.component';
-import {ProfileService} from './data/services/profile.service';
-import {JsonPipe} from '@angular/common';
-import {Profile} from './data/interfaces/profile.interface';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [ProfileCardComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular-blog';
-  profiles: Profile[] = []
-  profileService = inject(ProfileService);
 
-  constructor() {
-    this.profileService.getTestAccounts().subscribe(val => {
-      this.profiles = val
-    })
-  }
 }
