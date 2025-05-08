c062427ab8a86edbb8292226cde4a1158b7666d0
Artur Agh <aghartur@gmail.com>
2025-05-08 19:13:16 +0200

    upload image part

---
+++ src/app/pages/settings-page/settings-page.component.ts
@@ -0,0 +1,64 @@
+import {Component, effect, inject} from '@angular/core';
+import {ProfileHeaderComponent} from '../../profile-header/profile-header.component';
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import {ProfileService} from '../../data/services/profile.service';
+import {firstValueFrom} from 'rxjs';
+
+@Component({
+  selector: 'app-settings-page',
+  imports: [
+    ProfileHeaderComponent,
+    ReactiveFormsModule
+  ],
+  templateUrl: './settings-page.component.html',
+  styleUrl: './settings-page.component.scss'
+})
+export class SettingsPageComponent {
+  formBuilder = inject(FormBuilder);
+  profileService = inject(ProfileService);
+
+  form = this.formBuilder.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    username: [{value: '', disabled: true}, Validators.required],
+    description: [''],
+    stack: ['',],
+  })
+
+  constructor() {
+    effect(() => {
+      // @ts-ignore
+      this.form.patchValue({
+        ...this.profileService.me(),
+        // @ts-ignore
+        stack: this.mergeStack(this.profileService.me()?.stack)
+      })
+    });
+  }
+
+  onSave() {
+    this.form.markAllAsTouched();
+    this.form.updateValueAndValidity();
+
+    if (this.form.invalid) return;
+
+    // @ts-ignore
+    firstValueFrom(this.profileService.patchProfile({
+      ...this.form.value,
+      stack: this.splitStack(this.form.value.stack)
+    }))
+
+  }
+
+  splitStack(stack: string | null | string[] | undefined) {
+    if (!stack) return []
+    if (Array.isArray(stack)) return stack;
+    return stack.split(',')
+  }
+
+  mergeStack(stack: string | null | string[] | undefined) {
+    if (!stack) return ''
+    if (Array.isArray(stack)) return stack.join(',')
+    return stack
+  }
+}
---
+++ src/app/pages/settings-page/upload-image/upload-image.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-image',
+  imports: [],
+  templateUrl: './upload-image.component.html',
+  styleUrl: './upload-image.component.scss'
+})
+export class UploadImageComponent {
+
+}
